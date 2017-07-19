202f165050fa966085f7e5e59eddd4f2c306e1ca
ciatechsupport <rezendescullen@gmail.com>
2017-07-19 15:36:35 -0400

    First upload

---
+++ Service Tag Retriever/Service Tag Retriever/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Service_Tag_Retriever
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RetrieveData_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Hardware.GetBIOSserNo();
+            textBox2.Text = Hardware.GetUser();
+
+           /* if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, result);
+            }
+            */
+        }
+
+        private void openLocation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string result = "Service Tag:  " + textBox1.Text + "    User:  " + textBox2.Text;
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, result);
+                textBox3.Text = "File Created!";
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Process.Start("notepad.exe", saveFileDialog1.FileName);
+            }
+        }
+    }
+}
---
+++ Service Tag Retriever/Service Tag Retriever/Hardware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Diagnostics;
+
+namespace Service_Tag_Retriever
+{
+    class Hardware
+    {
+        public static string GetBIOSserNo()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BIOS");
+
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return wmi.GetPropertyValue("SerialNumber").ToString();
+
+                }
+
+                catch { }
+            }
+            return "BIOS Serial Number: Unknown";
+        }
+
+        public static string GetUser()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Username FROM Win32_ComputerSystem");
+            ManagementObjectCollection collection = searcher.Get();
+            string username = (string)collection.Cast<ManagementBaseObject>().First()["UserName"];
+            return username;
+            
+        }
+     
+    }
+}
